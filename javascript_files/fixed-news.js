ed64255376098ca22abba70085f9631d123a17a8
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2022-07-30 15:59:27 -0300

    ajuste

--- javascript_files/fixed-news.js
+++ javascript_files/fixed-news.js
@@ -21,7 +21,7 @@ export const fixedNews = [
       "subTitle": "Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias",
    },
    {
-      "id": 1,
+      "id": 4,
       "minTitle": "Festividade dos Jovens dia 14/09",
       "title": "Venha participar da nossa festividade!",
       "imageUrl": "../imagens/fixed-news-images/header.jpg",
