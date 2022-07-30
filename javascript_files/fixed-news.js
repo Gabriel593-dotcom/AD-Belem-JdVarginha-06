c83d181f89c4802b7b3884b7edbbc33ff7cf74da
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2022-07-29 22:25:18 -0300

    ajuste nas news

--- javascript_files/fixed-news.js
+++ javascript_files/fixed-news.js
@@ -3,21 +3,21 @@ export const fixedNews = [
     "id": 1,
     "minTitle" : "Festividade Jovens", 
     "title" : "Venha participar da nossa festividade!",
-    "imageUrl" : "../imagens/header.jpg",
+    "imageUrl" : "../imagens/fixed-news-images/header.jpg",
     "subTitle" : "Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias", 
    },
    {
     "id": 2,
     "minTitle" : "Teste", 
     "title" : "Teste para o load das fixed news",
-    "imageUrl" : "../imagens/header.jpg",
+    "imageUrl" : "../imagens/fixed-news-images/header.jpg",
     "subTitle" : "Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias",
    },
    {
       "id": 3,
       "minTitle" : "Teste", 
       "title" : "Teste Teste para o load das fixed news",
-      "imageUrl" : "../imagens/header.jpg",
+      "imageUrl" : "../imagens/fixed-news-images/header.jpg",
       "subTitle" : "Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias. Teste para o load das fixed news no clique dos elementos pinados no início do feed de notícias",
      }
 ];
