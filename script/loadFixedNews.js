a97e9e2da80dcd2dc5a4dbb34d08216ca455566d
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2022-07-30 15:43:39 -0300

    atualizando layout das fixed news

--- script/loadFixedNews.js
+++ script/loadFixedNews.js
@@ -1,9 +1,17 @@
 import { openModal } from "./loadFixedNewsModal.js";
 
+let fixedNewsBackgroundColors = ["#1abc9c", "#34495e", "#2c3e50", "#c0392b", "#e74c3c", "#2980b9", "#3498db"];
+
 export function createFixedNewsElements(parentElement, fixedNews) {
+    chooseColor();
     fixedNews.forEach(fixedNewsEl => {    
         let fixedNewsComponent = document.createElement("div");
         fixedNewsComponent.setAttribute("class", "fixed-news");
+        fixedNewsComponent.setAttribute("style", "background-color: " + fixedNewsBackgroundColors[chooseColor()]);
+        let fixedNewsComponentText = document.createElement("p");
+        fixedNewsComponentText.setAttribute("class", "fixed-news-min-title");
+        fixedNewsComponentText.innerText = fixedNewsEl.minTitle;
+        fixedNewsComponent.append(fixedNewsComponentText);
         openModalEvent(fixedNewsComponent, fixedNewsEl);
         parentElement.append(fixedNewsComponent);
     });
@@ -13,4 +21,8 @@ function openModalEvent(fixedNewsComponent, fixedNewsEl){
     fixedNewsComponent.addEventListener("click", () => {
         openModal(fixedNewsEl);
     });
+}
+
+function chooseColor() {
+    return Math.floor(Math.random() * fixedNewsBackgroundColors.length);
 }
